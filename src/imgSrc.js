8f4d02ea2d2dd8604d12e1e7a1f75515bf6d5076
alex <alexlhliang@gmail.com>
2021-09-14 16:45:00 -0700

    optimized images

--- src/imgSrc.js
+++ src/imgSrc.js
@@ -4,13 +4,13 @@ import woof from './assets/characters/dog.png';
 import wizard from './assets/characters/wizard.png';
 import odlaw from './assets/characters/evil_waldo.png';
 
-import levelOne from './assets/levels/1*7v_75ZGg1CTmWAw1rEgMHQ.jpeg';
-import levelTwo from './assets/levels/wheres-waldo-1.jpeg';
-import levelThree from './assets/levels/wheres-waldo-2.jpeg';
-import levelFour from './assets/levels/wheres-waldo-3.jpeg';
-import levelFive from './assets/levels/wheres-waldo-4.jpeg';
-import levelSix from './assets/levels/wheres-waldo-5.jpeg';
-import levelSeven from './assets/levels/wheres-waldo-6.jpeg';
+import levelOne from './assets/levels/opt_1*7v_75ZGg1CTmWAw1rEgMHQ.jpeg';
+import levelTwo from './assets/levels/opt_wheres-waldo-1.jpeg';
+import levelThree from './assets/levels/opt_wheres-waldo-2.jpeg';
+import levelFour from './assets/levels/opt_wheres-waldo-3.jpeg';
+import levelFive from './assets/levels/opt_wheres-waldo-4.jpeg';
+import levelSix from './assets/levels/opt_wheres-waldo-5.jpeg';
+import levelSeven from './assets/levels/opt_wheres-waldo-6.jpeg';
 
 import logo from './assets/Wheres_Waldo_-_Logo.png';
 
